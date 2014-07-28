3c7e5cec5f0c1df7ec60e94b41ea0f6eb76245f3
onechance <zhouh08@hotmail.com>
2014-07-28 22:13:08 +0800

    1

---
+++ Complex/scripts/initUIPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initUIPosition : MonoBehaviour {
+
+	private float targetX;  
+	private float targetY;  
+	
+	public Camera uiCam;
+
+	// Use this for initialization
+	void Start () { 
+
+		targetX = transform.localScale.x /2;
+
+		targetY = 20;
+
+		setScreenPosition(targetX, uiCam.pixelHeight-targetY);  
+	}
+	
+	void setScreenPosition(float x,float y)  
+	{  
+		Vector3 targetPos = uiCam.ScreenToWorldPoint(new Vector3(x, y, transform.position.z));  
+		Vector3 dir = targetPos - transform.position;  
+		transform.Translate(dir);  
+	}   
+}
