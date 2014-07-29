397f13a602f931aa02269b4ea62fc7f553a001f8
zhouhui <zhouh08@hotmail.com>
2014-07-29 16:23:08 +0800

    1

--- iTweenTest/course2/scripts/scene.cs
+++ iTweenTest/course2/scripts/scene.cs
@@ -6,6 +6,7 @@ public class scene : MonoBehaviour
 
 		private int size = 9;
 		public GameObject grid;
+		private GameObject preGrid;
 
 		// Use this for initialization
 		void Start ()
@@ -17,14 +18,42 @@ public class scene : MonoBehaviour
 		{
 				for (int i=0; i<size; i++) {
 						for (int j=0; j<size; j++) {
-								Instantiate (grid, new Vector3 (i, 0, j), Quaternion.identity);
+								GameObject go = (GameObject)Instantiate (grid, new Vector3 (i, 0, j), Quaternion.identity);
+								if ((i + j) % 2 == 0) {
+										iTween.ColorTo (go, Color.black, 0f);
+								}			
 						}
 				}	
+		}
+
+		void gridUp (GameObject go)
+		{
+				
+				if (preGrid != null && preGrid != go) {
+
+						if(preGrid.transform)
+
+						iTween.ColorTo (preGrid, Color.red, 0.5f);
+						iTween.MoveTo (preGrid, new Vector3 (preGrid.transform.position.x, 0f, preGrid.transform.position.z), 0.5f);	
+				} 
+			
+
+				iTween.ColorTo (go, Color.green, 0.5f);
+				iTween.MoveTo (go, new Vector3 (go.transform.position.x, 0.5f, go.transform.position.z), 0.5f);
+
+				preGrid = go;
+	
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-	
+				Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) {
+						if (hit.transform.tag == "grid") {
+								gridUp (hit.transform.gameObject);
+						}
+				}
 		}
 }
