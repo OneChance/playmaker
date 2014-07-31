f0d89ba1eb4e0e1899000d9478d937f77d1c6dbb
zhouhui <zhouh08@hotmail.com>
2014-07-31 14:37:47 +0800

    1

--- iTweenTest/course2/scripts/scene.cs
+++ iTweenTest/course2/scripts/scene.cs
@@ -7,11 +7,20 @@ public class scene : MonoBehaviour
 		private int size = 9;
 		public GameObject grid;
 		private GameObject preGrid;
+		private GameObject ballGrid;
+		public GameObject ball;
+		Vector3[] pathPoints;
+		int pointIndex;
+		Hashtable getState = new Hashtable ();
 
 		// Use this for initialization
 		void Start ()
 		{
 				createPlane ();
+				pathPoints = new Vector3[2];
+				
+				ballGrid = GameObject.FindGameObjectsWithTag ("grid") [0];
+				setGridDisable (ballGrid);
 		}
 
 		void createPlane ()
@@ -26,34 +35,77 @@ public class scene : MonoBehaviour
 				}	
 		}
 
-		void gridUp (GameObject go)
+		void gridStateBack (GameObject go)
 		{
-				
-				if (preGrid != null && preGrid != go) {
+				if ((go.transform.position.x + go.transform.position.z) % 2 == 0) {
+						iTween.ColorTo (go, Color.black, 0.2f);
+				} else {
+						iTween.ColorTo (go, Color.white, 0.2f);
+				}
+		
+				iTween.MoveTo (go, new Vector3 (go.transform.position.x, 0f, go.transform.position.z), 0.2f);	
+		}
 
-						if(preGrid.transform)
-
-						iTween.ColorTo (preGrid, Color.red, 0.5f);
-						iTween.MoveTo (preGrid, new Vector3 (preGrid.transform.position.x, 0f, preGrid.transform.position.z), 0.5f);	
+		void gridUp (GameObject go)
+		{			
+				if (preGrid != null && preGrid != go && ballGrid != preGrid) {
+						gridStateBack (preGrid);
 				} 
 			
-
-				iTween.ColorTo (go, Color.green, 0.5f);
-				iTween.MoveTo (go, new Vector3 (go.transform.position.x, 0.5f, go.transform.position.z), 0.5f);
-
-				preGrid = go;
-	
+				if (go.transform.position.y == 0) {					
+						if (ballGrid == null || ballGrid != go) {	
+								iTween.ColorTo (go, Color.green, 0.2f);
+								iTween.MoveTo (go, new Vector3 (go.transform.position.x, 0.5f, go.transform.position.z), 0.2f);		
+								preGrid = go;
+						}
+				}
 		}
-	
+
+		void setGridDisable (GameObject go)
+		{
+				iTween.ColorTo (go, Color.red, 0.01f);
+				if (go.transform.position.y != 0) {
+						iTween.MoveTo (go, new Vector3 (go.transform.position.x, 0f, go.transform.position.z), 0.2f);		
+				}
+		}
+
+		void moveToPoint ()
+		{	
+				if (pointIndex < 2) {
+						iTween.MoveTo (ball, iTween.Hash ("position", pathPoints [pointIndex], "speed", 10f, "easetype", "linear", "oncomplete", "moveToPoint", "oncompletetarget", this.gameObject));					
+						pointIndex++;		
+				}
+		}
+
 		// Update is called once per frame
 		void Update ()
 		{
 				Ray ray = camera.ScreenPointToRay (Input.mousePosition);
 				RaycastHit hit;
+
 				if (Physics.Raycast (ray, out hit)) {
-						if (hit.transform.tag == "grid") {
-								gridUp (hit.transform.gameObject);
+						if (hit.transform.tag == "grid") {	
+
+								gridUp (hit.transform.gameObject);	
+	
+								if (Input.GetMouseButtonDown (0)) {
+
+										if (ballGrid != null) {
+												gridStateBack (ballGrid);
+										}
+						
+										pointIndex = 0;
+										pathPoints [0] = new Vector3 (hit.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+										pathPoints [1] = new Vector3 (hit.transform.position.x, ball.transform.position.y, hit.transform.position.z);
+										moveToPoint ();
+										ballGrid = hit.transform.gameObject;	
+										setGridDisable (ballGrid);
+								}											
 						}
+				} else {
+						if (preGrid != null) {
+								gridStateBack (preGrid);
+						}		
 				}
 		}
 }
---
+++ iTweenTest/course3/scripts/moveSight.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveSight : MonoBehaviour
+{
+
+		public GameObject target;
+		private Vector3[] paths = new Vector3[3];
+		public GameObject ball;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) {
+						if (hit.transform.gameObject.tag == "grid") {
+								iTween.MoveUpdate (target, new Vector3 (hit.point.x, target.transform.position.y, hit.point.z), .1f);
+								if (Input.GetMouseButtonDown (0)) {
+										GameObject oneBall = (GameObject)Instantiate (ball, new Vector3 (0, 0, 0), Quaternion.identity);
+										paths [0] = new Vector3 (0, 0, 0);
+										paths [2] = hit.point;
+										paths [1] = new Vector3 (paths [1].x / 2, 3, paths [2].z / 2);
+										iTween.MoveTo (oneBall, iTween.Hash ("path", paths, "movetopath", true, "orienttopath", true, "time", 1, "easetype", iTween.EaseType.linear));
+										Destroy (oneBall, 2f);				
+								}			
+						}		
+				}
+		}
+}
