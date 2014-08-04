f2f7484396bee4ab4ed8359b244184ad85f91589
zhouhui <zhouh08@hotmail.com>
2014-08-04 16:56:47 +0800

    d

---
+++ C#Test/moveCube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCube : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.R)) {
+						gameObject.renderer.material.color = Color.red;	
+				}
+				if (Input.GetKeyDown (KeyCode.B)) {
+						gameObject.renderer.material.color = Color.blue;		
+				}
+				if (Input.GetKeyDown (KeyCode.G)) {
+						gameObject.renderer.material.color = Color.green;		
+				}
+		}
+
+		void OnMouseDown ()
+		{
+				Debug.Log ("mouse down");
+				rigidbody.AddForce (transform.forward * 100);
+		}
+}
---
+++ C#Test/singleInstance.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleInstance : MonoBehaviour
+{
+
+		private static SingleInstance instance;
+
+		public SingleInstance getInstance ()
+		{
+				if (instance == null) {
+						instance = new GameObject ("GameManager").AddComponent<SingleInstance> ();		
+				}	
+				return instance;
+		}
+
+		// Use this for initialization
+		void Awake ()
+		{
+				if (instance == null) {
+						instance = this;
+				} 
+				DontDestroyOnLoad (gameObject);	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
