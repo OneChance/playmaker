6a5b9467f0b5a00333258429a6831e3f9a64bb52
zhouhui <zhouh08@hotmail.com>
2014-08-13 15:46:37 +0800

    1

---
+++ 2DGame2/Scripts/MaterialScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialScripts : MonoBehaviour {
+
+	public int col = 5;
+	public int row = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
